403c2c746fafe67d6933f6cb39e26790ecf62641
federicogiustii <153283171+federicogiustii@users.noreply.github.com>
2025-06-07 10:25:25 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 import requests
+import random
 
 request = {
-    "M": "Ciao CarbonShift!",
-    "D": "2025-06-06T23:59:59",  # Data mock
-    "C": "http://localhost:5001/callback"
+    "M": "Ciao CarbonShift!",   # Message
+    "D": random.randint(0, 4),  # Deadline
+    "C": "http://localhost:5001/callback" # Callback
 }
 
 response = requests.post("http://localhost:5000/request", json=request)
